6e55fde192c65c869daeebe7eea48cde2fe97040
Naufal Levi Sabili <levisabili6@gmail.com>
2023-11-17 00:13:09 +0700

    commit bili

--- application/views/Auth/gantipass.php
+++ application/views/Auth/gantipass.php
@@ -1,57 +1,78 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="This is a login page template based on Bootstrap 5">
-    <title>Login SMK-TI GNC</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="<?php echo base_url('assets/images/logo.png') ?>" type="image/x-icon">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/login.css')?>">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>SMK-TI GNC</title>
 </head>
 
 <body>
-    <img class="wave" src="<?php echo base_url('assets/images/wave2.png')?>">
-    <div class="container">
-        <div class="img">
-            <img src="<?php echo base_url('assets/images/guru.png')?>">
-        </div>
-        <div class="login-content">
-            <form action="<?= base_url('auth/editpass/' . $id_users) ?>" method="post">
-                <img src="<?php echo base_url('assets/images/logo.png') ?>">
-                <h2 class="title">Ganti Password</h2>
-                <?php if(isset($error) && !empty($error)): ?>
-                <div class="alert alert-danger" role="alert"><?= $error ?></div>
-                <?php endif; ?>
-                <div class="input-div pass">
-                    <div class="i">
-                        <i class="fas fa-lock"></i>
+    <div class="container-fluid p-0" style="background-color: #ffc107; height: 50vh">
+        <div class="container d-flex justify-content-center align-items-center" style="height: 100vh">
+            <div class="card"
+                style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); border-radius: 10px 10px 10px 10px; width: 30%; height: 57%">
+                <form action="<?= base_url('auth/editpass/' . $id_users) ?>" method="post">
+                    <div class="card-body">
+                        <div class="row justify-content-center">
+                            <img src="<?php echo base_url('assets/images/logo.png') ?>" alt=""
+                                style="height: 100px; width: 130px;">
+                        </div>
+                        <div class="row text-center mt-2">
+                            <h3 class="fw-bold">Reset Password</h3>
+                        </div>
+                        <hr>
+                        <div class="row mb-3">
+                            <?php if(isset($error) && !empty($error)): ?>
+                            <div class="alert alert-danger" role="alert"><?= $error ?></div>
+                            <?php endif; ?>
+                        </div>
+                        <div class="row mb-4">
+                            <div class="col">
+                                <span>Password Baru</span>
+                                <input type="password" class="input form-control" id="newPassword" name="password"
+                                    required>
+                            </div>
+                        </div>
+                        <div class="row mb-4">
+                            <div class="col">
+                                <span>Konfirmasi Password</span>
+                                <input type="password" class="input form-control" id="confirmPassword"
+                                    name="confirm_password" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary" value="Submit">Gassss!!!</button>
+                            </div>
+                        </div>
                     </div>
-                    <div class="div">
-                        <h5>Password Baru</h5>
-                        <input type="password" class="input" id="newPassword" name="password" required>
-                    </div>
-                </div>
-                <div class="input-div pass">
-                    <div class="i">
-                        <i class="fas fa-lock"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Konfirmasi Password Baru</h5>
-                        <input type="password" class="input" id="confirmPassword" name="confirm_password" required>
-                    </div>
-                </div>
-                <input type="submit" class="btn" value="Submit">
-            </form>
-
+                </form>
+            </div>
         </div>
     </div>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/login.js')?>"></script>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 
 </body>
 
